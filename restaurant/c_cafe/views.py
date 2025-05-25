636d967836f0b48d5b97d94a2ed80187551128f8
shyam <shhyammm.prasad@gmail.com>
2025-05-25 17:26:47 +0000

    created base html page

--- restaurant/c_cafe/views.py
+++ restaurant/c_cafe/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views import View
+
 class HomeView(View):
     
     def get(self, request,*args,**kwargs):
 
-        return render(request, 'c_cafe/home.html')
+        return render(request, 'c_cafe/base.html')
     
