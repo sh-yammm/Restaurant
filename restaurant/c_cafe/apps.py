98f0df5642719603208e1bc7d0acbd608aa1b7db
shyam <shhyammm.prasad@gmail.com>
2025-05-25 16:54:16 +0000

    Initial commit

---
+++ restaurant/c_cafe/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CCafeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'c_cafe'
---
+++ restaurant/c_cafe/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views import View
+class HomeView(View):
+    
+    def get(self, request,*args,**kwargs):
+
+        return render(request, 'c_cafe/home.html')
+    
